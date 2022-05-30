afdbfc2cffe8d1b2cec45d030c8ad1ee47f16266
espoal <alberto.websci@gmail.com>
2022-05-30 11:46:29 +0200

    feat(loop): added loop mvp

---
+++ pkgs/loop/index.mjs
@@ -0,0 +1,56 @@
+import { parentPort } from 'node:worker_threads'
+import { arenaAllocatorFactory } from '@pkgs/allocator'
+import { io_uringFactory } from '@pkgs/io_uring'
+import { dispatcherFactory } from '@pkgs/dispatcher'
+
+// Setup memory allocator
+const {
+  allocator
+} = await arenaAllocatorFactory({
+
+})
+
+// Setup io_uring
+const {
+  readRing,
+  writeRing,
+  notifyRing
+} = await io_uringFactory({
+  allocator
+})
+
+// Start listening for messages
+const {
+  getRequests,
+  writeResponses,
+  callBackMap,
+  notifyDispatcher
+} = await dispatcherFactory({
+  parentPort
+})
+
+let shouldContinue = true
+let loopCount = 0
+
+while ( shouldContinue ) {
+  loopCount++
+
+  // Read batch of ready messages
+  const responses = await readRing()
+  shouldContinue = await writeResponses({ responses, callBackMap })
+
+  // Submit batch or requests
+  const requests = await getRequests({ responses, callBackMap })
+  await writeRing(...requests)
+  shouldContinue = await notifyRing(requests.length)
+}
+
+// Teardown
+await notifyDispatcher('Closing...')
+await notifyRing('Closing...')
+console.log(`Performed ${loopCount} loops.`)
+
+
+process.catch(() => {
+  shouldContinue = false
+})
